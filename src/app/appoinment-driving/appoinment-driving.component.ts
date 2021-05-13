a609b5873dddee8734561701d5ed65077f83dccd
Chameli <chame9dil@gmail.com>
2021-05-13 16:14:58 +0530

    fix bug in navbar

---
+++ src/app/appoinment-driving/appoinment-driving.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appoinment-driving',
+  templateUrl: './appoinment-driving.component.html',
+  styleUrls: ['./appoinment-driving.component.css']
+})
+export class AppoinmentDrivingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
