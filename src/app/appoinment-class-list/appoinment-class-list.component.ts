9a91097834442d20863e1001cebeb1d9a09f4342
Chameli <chame9dil@gmail.com>
2021-05-14 13:16:48 +0530

    add appoinment maker for theory classes

---
+++ src/app/appoinment-class-list/appoinment-class-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appoinment-class-list',
+  templateUrl: './appoinment-class-list.component.html',
+  styleUrls: ['./appoinment-class-list.component.css']
+})
+export class AppoinmentClassListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/appoinment-classes/appoinment-classes.component.ts
+++ src/app/appoinment-classes/appoinment-classes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppoinmentClssPayloads } from '../appoinment-clss-payloads';
+import { AppoinmentClssPayloadsService } from '../appoinment-clss-payloads.service';
 
 @Component({
   selector: 'app-appoinment-classes',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppoinmentClassesComponent implements OnInit {
 
-  constructor() { }
-
+  
+  appoinments:AppoinmentClssPayloads = new AppoinmentClssPayloads
+  
+  constructor(private appoinmentClassService:AppoinmentClssPayloadsService ,
+    private router:Router){}
+  
   ngOnInit(): void {
   }
+
+  saveAppoinmentClass(){
+    this.appoinmentClassService.createAppoinmentClss(this.appoinments).subscribe(data =>{
+      console.log(data);
+      this.goToAppoinmentsClssList();
+
+    }),
+    error => console.log(error);
+    
+  }
+  goToAppoinmentsClssList(){
+    this.router.navigate(['/appoinmentClassList']);
+  }
+
+  onCheack(){
+
+  }
+
+  onSubmit(){
+    console.log(this.appoinments);
+    this.saveAppoinmentClass();
+
+  }
+
 
 }
